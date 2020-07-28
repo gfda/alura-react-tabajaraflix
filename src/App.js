3e10342ad38a1d885a9ddb93e913e0536aa068f9
Gustavo Dias A <gfdiasa@gmail.com>
2020-07-28 18:53:15 -0300

    Fix import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain  from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Menu />
-      
+      <BannerMain />
+      <Carousel />
+      <Footer />
     </div>
   );
 }
