af843a4c558726ee7248ba2ef44e7308d63565c8
Gustavo Dias A <gfdiasa@gmail.com>
2020-07-27 23:32:55 -0300

    Update features

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
 
 function Menu() {
     return (
@@ -10,9 +11,9 @@ function Menu() {
                 <img className="Logo" src={Logo} alt="Tabajaraflix logo"/>
             </a>
 
-            <ButtonLink className="ButtonLink" href="/">
+            <Button as="a" className="ButtonLink" href="/">
                 Novo vídeo
-            </ButtonLink>
+            </Button>
         </nav>
     );
 }
