960507ef6e52c3d16adf2f30a4425f800e753cf0
Gustavo Dias A <gfdiasa@gmail.com>
2020-07-28 21:43:44 -0300

    Change carousel

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import Home from './pages/Home';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/cadastro/video" component={CadastroVideo}/>
       <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>    
+
       <Route component={() => (<div>404 - Not Found</div>)} />
     </Switch>
    </BrowserRouter>,
