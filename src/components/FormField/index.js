4b42582850ce858feaa56e6b01bbaff67a04eee2
Gustavo Dias A <gfdiasa@gmail.com>
2020-07-30 19:31:48 -0300

    Fix indentation

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -3,16 +3,16 @@ import React from 'react';
 function FormField({ label, type, name, value, onChange }) {
     return (
         <div>
-         <label>
-             {label}
-             <input 
-               type={type}
-               name={name} 
-               value={value}
-               onChange={onChange}
-             />
-         </label>
-         </div>
+            <label>
+                {label}
+                <input 
+                    type={type}
+                    name={name} 
+                    value={value}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
     );
 }
 export default FormField;
