aa89d075e34b1bd86f6c046315acfcb1148c1b29
Gustavo Dias A <gfdiasa@gmail.com>
2020-08-01 16:25:12 -0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
@@ -10,11 +10,11 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/cadastro/video" component={CadastroVideo}/>
-      <Route path="/cadastro/categoria" component={CadastroCategoria}/>    
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
 
       <Route component={() => (<div>404 - Not Found</div>)} />
     </Switch>
-   </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
